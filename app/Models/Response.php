7a3e03eaf32f01e01f67022a83f099220d2a6490
belal-allala <belalallala810@gmail.com>
2025-03-24 10:13:18 +0000

    feat: Create Response model, migration, and relationships

--- app/Models/Response.php
+++ app/Models/Response.php
@@ -4,8 +4,31 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Response extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'content',
+        'ticket_id',
+        'user_id',
+    ];
+
+    /**
+     * Get the ticket that owns the response.
+     */
+    public function ticket(): BelongsTo
+    {
+        return $this->belongsTo(Ticket::class);
+    }
+
+    /**
+     * Get the user that owns the response.
+     */
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
